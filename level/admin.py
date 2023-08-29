5c5a2b2654a69f79dd8c75597aa6320c541fe829
Tayeb EL OUADRASSI TLIDI <t.elouadrassi@gmail.com>
2023-08-29 13:22:35 +0100

    Level points, player points and region points are now automatically updated when ranking is changed

--- level/admin.py
+++ level/admin.py
@@ -2,20 +2,20 @@ from django.contrib import admin
 from adminsortable2.admin import SortableAdminMixin
 from .models import Level
 import math
-from django.db.models.signals import pre_save, post_save
-from django.dispatch import receiver
 
 # Register your models here.
 
 class CustomSortableAdminMixin(SortableAdminMixin):
     def _update_order(self, updated_items, extra_model_filters):
-
-        # Call the super to perform the update
         super()._update_order(updated_items, extra_model_filters)
-
         for item in updated_items:
             level = Level.objects.get(pk=item[0])
             level.save()
+        for level_record in level.levelrecord_set.all():
+            level_record.save()
+        for history_position in level.historyposition_set.all():
+            history_position.save()
+                
 
 class LevelAdmin(CustomSortableAdminMixin, admin.ModelAdmin):
     list_display = ['ranking', 'name', 'points', 'min_points', 'min_completion', 'first_victor']
