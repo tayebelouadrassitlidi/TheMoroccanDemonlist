ec25bdc1390e2c185afbcabcc5fa08b7673bff22
Tayeb EL OUADRASSI TLIDI <t.elouadrassi@gmail.com>
2023-08-31 02:20:02 +0100

    Removed Vercel Support (again)

--- themoroccandemonlist/settings.py
+++ themoroccandemonlist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-b0_686mpd0pv1%osm9u9pwnp^ilj*3#i5r1a)wue6-!r3$=6-=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
