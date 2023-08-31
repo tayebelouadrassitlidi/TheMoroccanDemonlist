dacc4baeff7221e62c2ab242e1516c5ec6f193f9
Tayeb EL OUADRASSI TLIDI <t.elouadrassi@gmail.com>
2023-08-31 02:48:15 +0100

    Added whitenoise

--- themoroccandemonlist/settings.py
+++ themoroccandemonlist/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'listchange',
     'django_extensions',
     'adminsortable2',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'themoroccandemonlist.urls'
